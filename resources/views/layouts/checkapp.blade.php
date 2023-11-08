66e3522cdf403cff87a668a4e060a38e6e6649f7
Saurav Neupane <sauravneupane43@gmail.com>
2023-11-08 11:25:41 +0545

    first commit

---
+++ resources/views/layouts/checkapp.blade.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- our css and javascript files -->
+    <link rel="stylesheet" href="{{asset('assets/css/custom-bs.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/jquery.fancybox.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap-select.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/fonts/icomoon/style.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/fonts/line-icons/style.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/animate.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/quill.snow.css')}}">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <div id="app">
+
+
+            <!-- NAVBAR -->
+    <header class="site-navbar mt-3">
+        <div class="container-fluid">
+          <div class="row align-items-center">
+            <div class="site-logo col-6"><a href="{{ url('/') }}">JobBoard</a></div>
+
+            <nav class="mx-auto site-navigation">
+              <ul class="site-menu js-clone-nav d-none d-xl-block ml-0 pl-0">
+                <li><a href="index.html" class="nav-link active">Home</a></li>
+                <li><a href="about.html">About</a></li>
+
+                <li><a href="profile.html">Profile</a></li>
+
+                <li><a href="contact.html">Contact</a></li>
+                <li class="d-lg-none"><a href="post-job.html"><span class="mr-2">+</span> Post a Job</a></li>
+                @guest
+                    @if (Route::has('login'))
+                <li class="d-lg-none"><a href="login.html">Log In</a></li>
+                @endif
+                @guest
+                @if (Route::has('register'))
+                <li class="d-lg-none"><a href="login.html">Register</a></li>
+                @endif
+            @else
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }}
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        @endguest
+              </ul>
+            </nav>
+
+            <div class="right-cta-menu text-right d-flex aligin-items-center col-6">
+              <div class="ml-auto">
+                <a href="post-job.html" class="btn btn-outline-white border-width-2 d-none d-lg-inline-block"><span class="mr-2 icon-add"></span>Post a Job</a>
+
+              </div>
+              <a href="#" class="site-menu-toggle js-menu-toggle d-inline-block d-xl-none mt-lg-2 ml-3"><span class="icon-menu h3 m-0 p-0 mt-2"></span></a>
+            </div>
+
+          </div>
+        </div>
+      </header>
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav> --}}
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+    <!-- SCRIPTS -->
+    <script src="{{asset('assets/js/jquery.min.js">')}}</script>
+    <script src="{{asset('assets/js/bootstrap.bundle.min.js">')}}</script>
+    <script src="{{asset('assets/js/isotope.pkgd.min.js">')}}</script>
+    <script src="{{asset('assets/js/stickyfill.min.js">')}}</script>
+    <script src="{{asset('assets/js/jquery.fancybox.min.js">')}}</script>
+    <script src="{{asset('assets/js/jquery.easing.1.3.js">')}}</script>
+
+    <script src="{{asset('assets/js/jquery.waypoints.min.js">')}}</script>
+    <script src="{{asset('assets/js/jquery.animateNumber.min.js">')}}</script>
+    <script src="{{asset('assets/js/owl.carousel.min.js">')}}</script>
+    <script src="{{asset('assets/js/quill.min.js">')}}</script>
+
+
+    <script src="{{asset('assets/js/bootstrap-select.min.js">')}}</script>
+
+    <script src="{{asset('assets/js/custom.js">')}}</script>
+</body>
+</html>
